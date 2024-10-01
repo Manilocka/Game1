53e0b8e8b6e0eacb3c7fc5c7f13ef05c7224afe4
Manilocka <ragozina2003@gmail.com>
2024-10-02 01:16:13 +0300

    level2

--- Assets/Scripts/level2.cs
+++ Assets/Scripts/level2.cs
@@ -1,18 +1,96 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class level2 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject grassPrefab;
+    public GameObject rockPrefab;
+    public GameObject treePrefab;
+    public GameObject platePrefab;
+    public GameObject characterPrefab;
+
+    private const int width = 10;
+    private const int height = 10;
+    private const float cellSize = 1.0f;
+
+    private string[,] boardLayout = new string[,]
+    {
+        {"G", "G", "G", "G", "G", "G", "G", "G", "G", "G"},
+        {"G", "P", "G", "G", "G", "R", "G", "G", "G", "G"},
+        {"G", "G", "G", "G", "T", "G", "G", "G", "G", "G"},
+        {"G", "G", "P", "G", "G", "R", "G", "G", "G", "G"},
+        {"G", "G", "G", "G", "G", "G", "G", "G", "G", "G"},
+        {"G", "G", "G", "G", "G", "G", "G", "G", "T", "G"},
+        {"G", "G", "R", "G", "C", "G", "G", "G", "G", "G"},
+        {"G", "G", "G", "G", "G", "G", "G", "P", "G", "G"},
+        {"G", "G", "G", "G", "G", "G", "G", "G", "G", "G"},
+        {"G", "G", "G", "G", "G", "G", "G", "G", "G", "G"},
+    };
+
     void Start()
     {
-        
+        GenerateBoard();
+        MoveCharacterToCenter();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void GenerateBoard()
     {
-        
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Vector3 position = new Vector3(x * cellSize, y * cellSize, 0);
+                Instantiate(grassPrefab, position, Quaternion.identity);
+            }
+        }
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                string element = boardLayout[y, x];
+
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                {
+                    Instantiate(treePrefab, new Vector3(x * cellSize, y * cellSize, 0.1f), Quaternion.identity);
+                }
+                else
+                {
+                    if (element == "T")
+                    {
+                        Instantiate(treePrefab, new Vector3(x * cellSize, y * cellSize, 0.1f), Quaternion.identity);
+                    }
+                    else if (element == "P")
+                    {
+                        // Создаем плиту и устанавливаем её индекс
+                        Plate newPlate = Instantiate(platePrefab, new Vector3(x * cellSize, y * cellSize, 0.1f), Quaternion.identity).GetComponent<Plate>();
+                        newPlate.index = y * width + x; // Устанавливаем индекс для плиты
+                    }
+                    else if (element == "R")
+                    {
+                        Instantiate(rockPrefab, new Vector3(x * cellSize, y * cellSize, 0.1f), Quaternion.identity);
+                    }
+                    else if (element == "C")
+                    {
+                        Instantiate(characterPrefab, new Vector3(x * cellSize, y * cellSize, 0.1f), Quaternion.identity);
+                    }
+                }
+            }
+        }
+    }
+    
+    void MoveCharacterToCenter()
+    {
+        GameObject character = GameObject.FindGameObjectWithTag("Character"); 
+        if (character != null)
+        {
+            Vector3 centerPosition = new Vector3((width - 1) / 2.0f * cellSize, (height - 1) / 2.0f * cellSize, 1); 
+            character.transform.position = centerPosition;
+
+            character.transform.position = new Vector3(
+                Mathf.Clamp(character.transform.position.x, -100 + cellSize / 2, width * cellSize + 100 - cellSize / 2),
+                Mathf.Clamp(character.transform.position.y, -100 + cellSize / 2, height * cellSize + 100 - cellSize / 2),
+                character.transform.position.z);
+        }
     }
 }
